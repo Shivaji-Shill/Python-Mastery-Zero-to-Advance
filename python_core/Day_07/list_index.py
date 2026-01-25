92eb4b9cf8315c50f5f456e01da180633cde8efc
Shivaji Shill <agrojitshill@gmail.com>
2026-01-25 23:18:28 +0600

    Add files via upload

---
+++ python_core/Day_07/list_index.py
@@ -0,0 +1,18 @@
+
+
+mobiles = ['iPhone', 'Samsung', 'OnePlus', 'Nokia', 'Sony']
+
+print(mobiles[2])  # Output: OnePlus
+
+print(mobiles[-1])  # Output: Sony
+
+mobiles.append('Motorola')
+print(mobiles)  # Output: ['iPhone', 'Samsung', 'OnePlus', 'Nokia', 'Sony', 'Motorola']
+
+mobiles.remove('Nokia')
+print(mobiles)  # Output: ['iPhone', 'Samsung', 'OnePlus', 'Sony', 'Motorola']
+
+# Real life use case:
+
+#Managing inventory of products
+#Organizing a reading list
---
+++ python_core/Day_07/practice.py
@@ -0,0 +1,18 @@
+# Create a list of your favorite foods
+foods = ["Rice", "Fish", "Chicken"]
+
+# Add one more food
+foods.append("Egg")
+
+# Remove one food
+foods.remove("Fish")
+
+# Print final list
+print(foods)
+
+# Print total food items
+print(len(foods))
+# Real life use case:
+#Meal planning
+#Grocery shopping list
+#Diet tracking
