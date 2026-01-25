60e44d0d2bb196cc9eaeddc55111b746808f6486
Shivaji Shill <agrojitshill@gmail.com>
2026-01-25 23:17:00 +0600

    Add files via upload

---
+++ python_core/Day_07/list.py
@@ -0,0 +1,10 @@
+#Creating a list of daily expenses
+daily_expenses = [50, 20, 15, 30, 10]
+
+#Printing the list of daily expenses
+print(daily_expenses)
+
+# Real life use case:
+#Calculate daily expenses
+#Product price list
+#Student marks
